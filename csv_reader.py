548f3cbd6b5dc30c0f02bce31c55b707c0d36d33
Joshua <jcal@itu.dk>
2025-03-10 19:00:46 +0100

    removed unusable code

--- csv_reader.py
+++ csv_reader.py
@@ -1,21 +1,8 @@
-import csv 
 import pandas as pd
 import csv_schema as schema
 import person as Person
 
-
 default_file_path = 'data/users.csv'
-
-def read_csv(file_path) -> None:
-    print(type(file_path))
-    if file_path == None:
-        file_path = default_file_path
-
-    with open(file_path, 'r') as file:
-        csv_reader = csv.reader(file)
-        for row in csv_reader:
-            if "Yes," in "Y":
-                print(row[2] + " " + row[3] + ", " + row[7])
 
 def read_csv_pd(file_path) -> dict[str, Person.Person]:
     if file_path == None:
