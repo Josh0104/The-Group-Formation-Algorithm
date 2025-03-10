96bce9ae50cb86c8d3ca47bf9180a6972fc0ec54
Joshua <jcal@itu.dk>
2025-03-10 14:19:53 +0100

    Some more init

--- csv_reader.py
+++ csv_reader.py
@@ -21,12 +21,12 @@ def read_csv_pd(file_path):
 
     df = pd.read_csv(file_path)
     # Filter the "First Name" column where the name contains "nd" (case-insensitive)
-    filtered_names = df[df[schema.columns["q1"]].str.contains("Yes", case=False, na=False)]
-
+    #filtered_names = df[df[schema.columns["q1"]].str.contains("Yes", case=False, na=False)]
+    filtered_names = df
     
     result = filtered_names[["First name", "Last name"]]
-    print(type(result))
     #Print each matching name
-    for name in filtered_names:
-        print(name)
+    for name in result.iterrows():
+        n = name
+        print(n[1][0] + " " + n[1][1])
 
