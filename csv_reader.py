83747ed0f1e5af006c6f4920dd3b518179cbc11c
Joshua <jcal@itu.dk>
2025-03-10 16:05:27 +0100

    extended the csv.reader.py file

--- csv_reader.py
+++ csv_reader.py
@@ -25,7 +25,23 @@ def read_csv_pd(file_path) -> dict[str, Person.Person]:
     #filtered_names = df[df[schema.columns["q1"]].str.contains("Yes", case=False, na=False)]
     filtered_names = df
     
-    result = filtered_names[["First name", "Last name"]]
+    result = filtered_names[[
+        schema.columns["first_name"], 
+        schema.columns["last_name"],
+        schema.columns["birthday"],
+        schema.columns["gender"],
+        schema.columns["country"],
+        schema.columns["q1"],
+        schema.columns["q2"],
+        schema.columns["q3"],
+        schema.columns["q4"],
+        schema.columns["q5"],
+        schema.columns["q6"],
+        schema.columns["q7"],
+        schema.columns["q8"],
+        schema.columns["q9"],
+        schema.columns["q10"],
+        ]]
     #Print each matching name
     i = 0
     for name in result.iterrows():
--- person.py
+++ person.py
@@ -1,8 +1,36 @@
+from datetime import date
+from enum import Enum
+
 class Person :
-    def __init__(self, id, name, age) :
+    def __init__(self, id, name, birthday, gender, country, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10):
         self.id = id
         self.name = name
-        self.age = age
+        self.birthday = birthday
+        self.gender = gender
+        self.country = country
+        self.a1 = a1
+        self.a2 = a2
+        self.a3 = a3
+        self.a4 = a4
+        self.a5 = a5
+        self.a6 = a6
+        self.a7 = a7
+        self.a8 = a8
+        self.a9 = a9
+        self.a10 = a10
 
     def say_hello(self) :
         print("Hello, my name is", self.name)
+
+    def calculate_age(self):
+        today = date(2025, 5, 28)
+        return today.year - self.birthday.year - ((today.month, today.day) < (self.birthday.month, self.birthday.day))
+
+class Gender(Enum):
+    MALE = 1
+    FEMALE = 2
+
+class SurveyResponse(Enum):
+    YES = 1
+    NO = 2
+    MAYBE = 3
